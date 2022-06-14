4eca2b998533fb4c6a49e0cba167e7ace558ed41
Serhii Kupriienko <skupriienko@anaconda.com>
2022-06-14 08:12:25 +0000

    Update bld script with usung ninja

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,19 +6,34 @@ if [[ `uname` == "Darwin" ]]; then
   EXTRA_CMAKE_ARGS="${EXTRA_CMAKE_ARGS} -DCMAKE_MACOSX_RPATH=ON"
 fi
 
-mkdir -p cmake-build
-cd cmake-build
+# Isolate the build.
+mkdir -p Build-${PKG_NAME}
+cd Build-${PKG_NAME} || exit 1
 
-cmake -DCMAKE_BUILD_TYPE=Release \
+# Generate the build files.
+echo "Generating the build files."
+
+cmake .. ${CMAKE_ARGS}                                   \
+      -GNinja                                            \
+      -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
-      -DCMAKE_C_COMPILER=${PREFIX} \
       -DCMAKE_EXE_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib" \
       ${EXTRA_CMAKE_ARGS} ${SRC_DIR}
 
-make -j${CPU_COUNT}
+# Build.
+echo "Building..."
+ninja -j${CPU_COUNT} || exit 1
 
-make -j${CPU_COUNT} test
+# Perform tests.
+echo "Testing..."
+ctest -VV --output-on-failure || true # there are failed tests
 
-make -j${CPU_COUNT} install
+# Installing
+echo "Installing..."
+ninja install || exit 1
+
+# Error free exit!
+echo "Error free exit!"
+exit 0
